9ae0aad027620b932797c29b67576b3cf65480ab
Péter Gombos <gombos@stud.ntnu.no>
2012-09-01 04:54:37 +0200

    added files christea

---
+++ ReadLevel.js
@@ -0,0 +1,25 @@
+function ReadLevel() {
+}
+
+ReadLevel.prototype.init = function() {
+	level_data = {"windmills": [[1,1],[2,2],[3,3]]};
+}
+
+ReadLevel.prototype.pause = function() {
+}
+
+ReadLevel.prototype.resume = function() {
+}
+
+ReadLevel.prototype.render = function(ctx) {
+	//Add code for reading json files
+	//Have added a thought of file for proof of concept
+	var x = 0;
+	var y = 0;
+	var img = new Image();
+	img.src = "/tiles.JPG";
+	ctx.drawImage(img,10,10);
+}
+
+ReadLevel.prototype.update = function() {
+}
