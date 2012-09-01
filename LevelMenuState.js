7a23fb0b2c8a3386fa3132b0226d9ade28265754
Stian Jensen <me@stianj.com>
2012-09-01 05:37:44 +0200

    Drawing level grid

--- LevelMenuState.js
+++ LevelMenuState.js
@@ -2,10 +2,32 @@ function LevelMenuState(){
 }
 
 LevelMenuState.prototype.init = function() {
+	this.numberOfLevels = 15;
+	this.elements = [];
+	this.levelTiles = [];
 }
 
 LevelMenuState.prototype.resume = function() {
+	for (var i=0;i<this.numberOfLevels;i++) {
+		this.levelTiles[i] = {x: 2.75+2.25*(i%5), y: 1.5+2.25*((i/5)|0), w:1.5, h:1.5, locked:true, stars:0};
+		this.elements.push([this.chooseLevel, this.levelTiles[i], i ]);
+	}
+	console.log(this.elements);
 }
 
 LevelMenuState.prototype.pause = function() {
+}
+
+LevelMenuState.prototype.update = function() {
+}
+LevelMenuState.prototype.render = function(ctx) { 
+	for (var i=0; i < this.levelTiles.length; i++) {
+		var levelTile = this.levelTiles[i];
+		ctx.fillStyle = 'blue';
+		ctx.fillRect(levelTile.x*GU, levelTile.y*GU, levelTile.w*GU, levelTile.h*GU);
+	}
+}
+
+LevelMenuState.prototype.chooseLevel = function() {
+	alert("You chose level " + (i+1) + "!");
 }
--- bootstrap.js
+++ bootstrap.js
@@ -88,6 +88,7 @@ function bootstrap(){
 	blurctx = blurcanvas.getContext("2d");
 	scanlinecanvas = document.createElement("canvas");
 	scanlinectx = scanlinecanvas.getContext("2d");
+	game_data = readData();
 
 	sm = new StateManager();
 	dt = 0;
@@ -154,6 +155,35 @@ function resize(e){
 	    scanlinectx.save();
 	    scanlinectx.scale(16*GU/1920,16*GU/1920);
 	    scanlinectx.restore();
+}
+
+function saveData(data) {
+	json_data = JSON.stringify(data);
+	setCookie("game_data", json_data, 10*5);
+}
+function readData() {
+	json_data = getCookie("game_data");
+	if (json_data !== undefined) {
+		return JSON.parse(json_data);
+	}
+}
+
+function setCookie(c_name,value,exdays) {
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie=c_name + "=" + c_value;
+}
+function getCookie(c_name) {
+	var i,x,y,ARRcookies=document.cookie.split(";");
+	for (i=0;i<ARRcookies.length;i++) {
+		x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+		y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+		x=x.replace(/^\s+|\s+$/g,"");
+ 		if (x==c_name) {
+			return unescape(y);
+		}
+	}
 }
 
 window.onresize = resize;
