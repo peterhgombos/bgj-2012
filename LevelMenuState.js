161553eba5cd5a9a73309caefb5ddf3c4484ef0a
Péter Gombos <gombos@stud.ntnu.no>
2012-09-01 06:52:49 +0200

    added back button in level menu

--- LevelMenuState.js
+++ LevelMenuState.js
@@ -12,6 +12,8 @@ LevelMenuState.prototype.resume = function() {
         this.levelTiles[i] = {x: 2.75+2.25*(i%5), y: 1.5+2.25*((i/5)|0), w:1.5, h:1.5, locked:true, stars:0};
         this.elements.push([this.chooseLevel, this.levelTiles[i], i ]);
     }
+    this.backButtonObject = {x: 0.5, y: 0.5, w: 1.5, h: 0.5}
+    this.elements.push([this.backButton,this.backButtonObject]) 
 }
 
 LevelMenuState.prototype.pause = function() {
@@ -30,8 +32,14 @@ LevelMenuState.prototype.render = function(ctx) {
         ctx.textAlign = 'center';
         ctx.fillText(i+1, (levelTile.x+levelTile.w/2)*GU, (levelTile.y+levelTile.h/10)*GU);
     }
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(this.backButtonObject.x*GU, this.backButtonObject.y*GU, this.backButtonObject.w*GU, this.backButtonObject.h*GU);
 }
 
 LevelMenuState.prototype.chooseLevel = function( params ) {
     alert("You chose level " + (params[0]+1) + "!");
+}
+
+LevelMenuState.prototype.backButton = function( params ) {
+    sm.changeState('mainmenu');
 }
