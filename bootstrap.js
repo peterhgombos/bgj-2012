154288c141dde11d36d6466a07a6ae2380b895bb
Stian Jensen <me@stianj.com>
2012-09-02 09:18:56 +0200

    Added scroll-preventiness for mobile devices

--- bootstrap.js
+++ bootstrap.js
@@ -177,8 +177,11 @@ function relMouseCoords(e){
 
 window.addEventListener('click', yo);
 window.addEventListener('touchstart', yo);
+window.addEventListener('touchstart', function(e){e.preventDefault();e.stopPropagation();return false;});
         
     function yo(e){
+        e.preventDefault();
+        e.stopPropagation();
 	    mouseXY = relMouseCoords(e);
         var clickables;
         if (sm.activeState.gameMenuWindow !== undefined && sm.activeState.gameMenuWindow.visible) {
@@ -197,6 +200,7 @@ window.addEventListener('touchstart', yo);
                 break;
 			}
 		}
+        return false;
 	}
 
 window.onresize = resize;
